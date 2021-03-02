6df5b3fe3fcb357c72ed13b09629a1eb65a2d19c
rliberty <roryliberty@gmail.com>
2021-03-02 16:21:03 -0600

    One Guy's Fugis is starting to form. Got a background image. That is all.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    mode: 'none',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
